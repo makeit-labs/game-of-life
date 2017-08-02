e3e3bcd395b11d3229f743d2b5ad4c8d5d42e30e
Shanfan Huang <shuang@pivotal.io>
2017-08-02 12:34:16 -0400

    Adding logic to the input and output

--- js/jasmine/spec/GenesisSpec.js
+++ js/jasmine/spec/GenesisSpec.js
@@ -1,77 +1,38 @@
-var seedBoard = function(rows, columns){
-  var board = [];
-  for (var i = 0; i < rows; i++) {
-    var row = [];
-    for (var j = 0; j < columns; j++) {
-      row.push(j == 0);
+var testCenter = function(threeByThree) {
+  var counter = 0;
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (threeByThree[i][j]) {
+        counter++;
+      }
     }
-    board.push(row);
   }
-  return board;
+  return counter > 2;
 }
-
-var applyRule1 = function(seedBoard) {
-  
-  return [[false, false, false],
-        [true, false, false],
-        [false, false, false]];
-}
-
-describe("Seeding the board", function() {
-
-  it("bears 3x3 lives", function() {
-    expect(seedBoard(3, 3)).toEqual([
-      [true, false, false],
-      [true, false, false],
-      [true, false, false]
-    ]);
-  });
-
-  it("bears 4x3 lives", function() {
-    expect(seedBoard(4, 3)).toEqual([
-      [true, false, false],
-      [true, false, false],
-      [true, false, false],
-      [true, false, false]
-    ]);
-  });
-
-  it("bears 3x4 lives", function() {
-    expect(seedBoard(3, 4)).toEqual([
-      [true, false, false, false],
-      [true, false, false, false],
-      [true, false, false, false]
-    ]);
-  });
-});
 
 describe("Playing the game of life", function(){
-  describe("dying when it has less than 2 neighbors", function(){
-    it("dies with a basic board", function(){
-      var basicBoard = [
-        [true, false, false],
-        [true, false, false],
-        [true, false, false]
-      ];
-      expect(applyRule1(basicBoard)).toEqual([
-        [false, false, false],
-        [true, false, false],
-        [false, false, false]
-      ]);
-    });
+  it("dies of under population", function(){
+    var basicBoard = [
+      [false, false, false],
+      [false, true, false],
+      [false, false, false]
+    ];
+    expect(testCenter(basicBoard)).toEqual(false);
 
-    it("dies with a mirrored board", function(){
-      var mirrorBoard = [
-        [false, false, true],
-        [false, false, true],
-        [false, false, true]
-      ];
-
-      expect(applyRule1(mirrorBoard)).toEqual([
-        [false, false, false],
-        [false, false, true],
-        [false, false, false]
-      ]);
-    });
+    basicBoard = [
+      [false, false, false],
+      [true, true, false],
+      [false, false, false]
+    ];
+    expect(testCenter(basicBoard)).toEqual(false);
   });
+
+  it("lives with 2 neighbors", function(){
+    var basicBoard = [
+      [false, false, false],
+      [true, true, true],
+      [false, false, false]
+    ];
+    expect(testCenter(basicBoard)).toEqual(true);
+  })
 });
